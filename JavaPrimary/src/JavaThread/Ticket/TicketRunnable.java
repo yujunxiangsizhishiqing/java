a8ad511a3b8250d22462ede12bc5de23a97c5be7
与君相思只是情 <76548129+yujunxiangsizhishiqing@users.noreply.github.com>
2021-10-23 15:46:25 +0800

    Update TicketRunnable.java

--- JavaPrimary/src/JavaThread/Ticket/TicketRunnable.java
+++ JavaPrimary/src/JavaThread/Ticket/TicketRunnable.java
@@ -1,5 +1,11 @@
 package JavaThread.Ticket;
 
+/*
+ * 使用接口的方式，每次只创建了一个共享对象，所有的线程能够实现资源共享
+ *      1、数据不一致的问题
+ *          解决方法：线程同步
+ *
+ */
 public class TicketRunnable implements Runnable {
     private int ticket = 5;
 
